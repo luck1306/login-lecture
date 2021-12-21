20966594be6cc9a848bce50d021c46c947c09035
luck1306 <81683553+luck1306@users.noreply.github.com>
2021-12-21 11:20:21 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//module
+const express = require("express");
+const app = express();
+
+//routing
+const home = require("./routes/home/index");
+
+//app setting
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드
+
+module.exports = app;
